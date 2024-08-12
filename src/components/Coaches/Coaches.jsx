6bb1dac9f7ad3b2fa4738b053356b264281720ca
Abhash Tiwari <abhashhtiwary7610@gmail.com>
2024-08-13 00:46:28 +0530

    mobile desktop media query added

--- src/components/Coaches/Coaches.jsx
+++ src/components/Coaches/Coaches.jsx
@@ -57,6 +57,7 @@ const Coaches = () => {
             animate={{ opacity: 1, x: 0 }}
             exit={{ opacity: 0, x: -100 }}
             transition={transition}
+            className='lp'
           >
             {coachesData[selected].description}
           </motion.p>
